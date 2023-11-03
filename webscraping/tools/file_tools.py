ec80ffc46ab52cd4fdd73b74a7313ed871fb3095
trulsbv <trulsvidnes@hotmail.no>
2023-11-03 10:00:39 +0100

    removed functions related to saving id in expired .csv

--- webscraping/tools/file_tools.py
+++ webscraping/tools/file_tools.py
@@ -61,58 +61,6 @@ def find_html(id, extension=".txt"):
         s+=line
     return s
 
-def find_name(i):
-    """
-    Takes an id and searches through known_ids.csv
-    to see if we already know the name of the id from a previous fetch
-    This is to reduce requests.
-
-    The file is structured:
-    ID;NAME;DATE
-
-    Input: 
-        - String id
-    
-    Output:
-        - String name or False
-    """
-    file = open("known_ids.csv", "r", encoding="UTF-8")
-    lines = file.readlines()
-    for line in lines:
-        id, name, _date = line.split(";")
-        date = _date.strip()
-        if id == i:
-            if date != str(d.today()):
-                prints.warning(message=f"{id}: The name '{name}' might be outdated", sender="file_tools\\find_name")
-            return name
-    return False
-
-def save_name(n, i):
-    """
-    Takes a name and id and saves it in known_ids.csv with todays date
-
-    Input: 
-        - String name
-        - String id
-    
-    Output: 
-        - None
-    """
-    file = "known_ids.csv"
-    fileLines = open(file, 'r', encoding="UTF-8").readlines()
-    modified = False
-    modifyLines = open(file, 'w', encoding="UTF-8")
-    for line in fileLines:
-        id, _name, _date = line.split(";")
-        if i == id:
-            modified = True
-            modifyLines.write(f"{i};{n};{d.today()}\n")
-        else:
-            modifyLines.write(line)
-    if not modified:
-        modifyLines.write(f"{i};{n};{d.today()}\n")
-    modifyLines.close()
-
 def save_html(id, html):
     """
     Takes a id and a string and saves it in [id].txt with todays date
